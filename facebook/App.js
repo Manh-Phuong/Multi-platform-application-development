a4c8c71f88a6c7f707bdf89a602f2fbdd293cb10
Manh-Phuong <phuong.nm23112002@gmail.com>
2023-10-02 01:08:19 +0700

    base

---
+++ facebook/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { publicRoutes } from "./src/routes";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        {publicRoutes.map((route, index) => (
+          <Stack.Screen
+            key={index}
+            name={route.name}
+            component={route.component}
+          />
+        ))}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ facebook/src/routes/index.js
@@ -0,0 +1,13 @@
+// publicRoutes.js
+import HomeScreen from "../screens/Home";
+
+const publicRoutes = [
+  {
+    name: "Home",
+    component: HomeScreen,
+  },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
