a4a749915f6def9d520931bdf6650bb9bebe7804
Manh-Phuong <phuong.nm23112002@gmail.com>
2023-11-20 09:55:28 +0700

    redux toolkit

--- facebook/App.js
+++ facebook/App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { publicRoutes } from "./src/routes";
 import { AuthProvider } from "./src/contexts/AuthContext";
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -16,21 +18,23 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <AuthProvider>
-        <Stack.Navigator
-          // initialRouteName="Home"
-          initialRouteName="Login" 
-          screenOptions={{ headerShown: false }}
-        >
-          {publicRoutes.map((route, index) => (
-            <Stack.Screen
-              key={index}
-              name={route.name}
-              component={route.component}
-            />
-          ))}
-        </Stack.Navigator>
-      </AuthProvider>
+      <Provider  store={store}>
+        <AuthProvider>
+          <Stack.Navigator
+            // initialRouteName="Home"
+            initialRouteName="Login" 
+            screenOptions={{ headerShown: false }}
+          >
+            {publicRoutes.map((route, index) => (
+              <Stack.Screen
+                key={index}
+                name={route.name}
+                component={route.component}
+              />
+            ))}
+          </Stack.Navigator>
+        </AuthProvider>
+      </Provider>
     </NavigationContainer>
   );
 }
